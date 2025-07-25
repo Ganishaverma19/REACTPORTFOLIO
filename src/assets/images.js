59792005250038d52bcd53b32f8dba395a39d06d
Ganisha Verma <22BET10279@cuchd.in>
2025-07-25 14:13:40 +0530

    Add  all images  assests

---
+++ src/assets/images.js
@@ -0,0 +1,18 @@
+export const Portfolio = [
+    require('./portfolio/360Nest1.png'),
+    require('./portfolio/360Nest2.png'),    
+    require('./portfolio/360Nest3.png'),
+    require('./portfolio/360Nest4.png'),
+];
+export const FuelHub = [ 
+    require('./portfolio/FuelHub1.png'),
+    require('./portfolio/FuelHub2.png'),
+    require('./portfolio/FuelHub3.png'),
+    require('./portfolio/FuelHub4.png'),
+];
+export const NEST= [
+    require('./portfolio/360Nest1.png'),
+    require('./portfolio/360Nest2.png'),
+    require('./portfolio/360Nest3.png'),
+    require('./portfolio/360Nest4.png'),
+];
